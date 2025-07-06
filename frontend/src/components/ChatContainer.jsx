c802592d2ec845501e0d471b274ed3692d0e6d47
Shashwat Dubey <i.shashwatdubey@gmail.com>
2025-07-06 22:19:55 +0530

    feat: add message time formatting utility and update ChatContainer to use it

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,6 +5,7 @@ import ChatHeader from "./ChatHeader.jsx";
 import MessageSkeleton from "./MessageSkeleton.jsx";
 import { useEffect } from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils.js";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
@@ -50,10 +51,10 @@ const ChatContainer = () => {
             </div>
             <div className="chat-header mb-1">
               <time className="text-xs opacity-50 ml-1">
-                {message.createdAt}
+                {formatMessageTime(message.createdAt)}
               </time>
             </div>
-            <div className="chat-bubble flex">
+            <div className="chat-bubble flex flex-col">
               {message.image && (
                 <img 
                 src={message.image}
