9ee6ca18e998e383c74a3f2cb8e93e2a5d124877
Shashwat Dubey <i.shashwatdubey@gmail.com>
2025-07-06 13:28:10 +0530

    feat: integrate Tailwind CSS and DaisyUI, remove App.css
    - Added Tailwind CSS and DaisyUI to the project dependencies.
    - Removed App.css and replaced it with Tailwind CSS imports in index.css.
    - Updated App.jsx to implement React Router for navigation between Home, Sign Up, Login, Settings, and Profile pages.
    - Created Navbar component for navigation.
    - Implemented basic structure for HomePage, LoginPage, SignUpPage, ProfilePage, and Settings components.
    - Set up Axios instance for API calls.
    - Established Zustand store for authentication state management.
    - Removed unused assets and cleaned up the project structure.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,20 +4,27 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
-  console.log('Server is running on Port: ' + PORT);
+  console.log("Server is running on Port: " + PORT);
   connectDB();
 });
